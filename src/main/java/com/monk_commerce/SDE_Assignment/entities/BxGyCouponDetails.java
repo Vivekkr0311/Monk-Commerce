11ba20bfa4a97402e4cb9c7a86978f9caf240869
Vivek Kumar <vivekkr0311@gmail.com>
2024-11-18 20:27:26 +0530

    BxGy null value fix

--- src/main/java/com/monk_commerce/SDE_Assignment/entities/BxGyCouponDetails.java
+++ src/main/java/com/monk_commerce/SDE_Assignment/entities/BxGyCouponDetails.java
@@ -1,5 +1,6 @@
 package com.monk_commerce.SDE_Assignment.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,12 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BxGyCouponDetails implements CouponDetails{
+    @JsonProperty("buy_products")
     private List<ProductQuantity> buyProducts;
+
+    @JsonProperty("get_products")
     private List<ProductQuantity> getProducts;
+
+    @JsonProperty("repition_limit")
     private int repetitionLimit;
 }
