e916fcf77d6a1502f02e491fbf32aed1febd63d3
Vivek Kumar <vivekkr0311@gmail.com>
2024-11-18 17:00:29 +0530

    Initial commit for the Monk Commerece

---
+++ src/main/java/com/monk_commerce/SDE_Assignment/SdeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.monk_commerce.SDE_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monk_commerce/SDE_Assignment/SdeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monk_commerce.SDE_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
